88c21a70ba6735d1c4507594e37f874b4d420ec0
parkdoyeon1 <parkdoyeon3239@gmail.com>
2023-08-02 23:09:16 +0900

    [ADD] Application

---
+++ Application/Sources/Application/PlanItApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct PlanItApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+        }
+    }
+}
